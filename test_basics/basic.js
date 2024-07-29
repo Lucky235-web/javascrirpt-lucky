ab86487075bbf526c727a46e07cc34b0af6457f9
Lucky235-web <chinu4173@gmail.com>
2024-07-29 08:25:18 +0000

    basic

---
+++ test_basics/basic.js
@@ -0,0 +1,6 @@
+const accountid =12345
+var accountEmail ="lucky4321@gmail"
+let accountPassword= "747474"
+accountcity ="delhi"
+
+console.table([accountid ,accountEmail ,accountPassword ,accountcity])
